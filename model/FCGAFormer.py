e33ae474e9d92ce9a508199f97600b2364e94e77
yangziyi <49685639+yangziyi999@users.noreply.github.com>
2023-05-15 10:58:46 +0800

    Add files via upload

---
+++ model/FCGAFormer.py
@@ -0,0 +1,416 @@
+import torch
+import torch.nn as nn
+
+from einops import rearrange
+from einops.layers.torch import Rearrange
+
+from fvcore.nn import FlopCountAnalysis, parameter_count_table
+
+
+def conv_3x3_bn(inp, oup, image_size, downsample=False):
+    stride = 1 if downsample == False else 2
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 7, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.GELU()
+    )
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn, norm):
+        super().__init__()
+        self.norm = norm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+# 未使用
+class SE(nn.Module):
+    def __init__(self, inp, oup, expansion=0.25):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(oup, int(inp * expansion), bias=False),
+            nn.GELU(),
+            nn.Linear(int(inp * expansion), oup, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+# CBAM注意力 ChannelAttention SpatialAttention cbam_block
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=8):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        # 利用1x1卷积代替全连接
+        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+
+class cbam_block(nn.Module):
+    def __init__(self, channel, ratio=8, kernel_size=7):
+        super(cbam_block, self).__init__()
+        self.channelattention = ChannelAttention(channel, ratio=ratio)
+        self.spatialattention = SpatialAttention(kernel_size=kernel_size)
+
+    def forward(self, x):
+        x = x * self.channelattention(x)
+        x = x * self.spatialattention(x)
+        return x
+
+
+class CA_Block(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(CA_Block, self).__init__()
+
+        self.conv_1x1 = nn.Conv2d(in_channels=channel, out_channels=channel // reduction, kernel_size=1, stride=1,
+                                  bias=False)
+
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm2d(channel // reduction)
+
+        self.F_h = nn.Conv2d(in_channels=channel // reduction, out_channels=channel, kernel_size=1, stride=1,
+                             bias=False)
+        self.F_w = nn.Conv2d(in_channels=channel // reduction, out_channels=channel, kernel_size=1, stride=1,
+                             bias=False)
+
+        self.sigmoid_h = nn.Sigmoid()
+        self.sigmoid_w = nn.Sigmoid()
+
+    def forward(self, x):
+        _, c, h, w = x.size()
+
+        x_h = torch.mean(x, dim=3, keepdim=True).permute(0, 1, 3, 2)
+        x_w = torch.mean(x, dim=2, keepdim=True)
+
+        x_cat_conv_relu = self.relu(self.bn(self.conv_1x1(torch.cat((x_h, x_w), 3))))
+        x_cat_conv_split_h, x_cat_conv_split_w = x_cat_conv_relu.split([h, w], 3)
+
+        s_h = self.sigmoid_h(self.F_h(x_cat_conv_split_h.permute(0, 1, 3, 2)))
+        s_w = self.sigmoid_w(self.F_w(x_cat_conv_split_w))
+
+        out = x * s_h.expand_as(x) * s_w.expand_as(x)
+        return out
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.2):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class MBConv(nn.Module):
+    def __init__(self, inp, oup, image_size, downsample=False, expansion=4):
+        super().__init__()
+        self.downsample = downsample
+        stride = 1 if self.downsample == False else 2
+        hidden_dim = int(inp * expansion)
+
+        if self.downsample:
+            self.pool = nn.MaxPool2d(3, 2, 1)
+            self.proj = nn.Conv2d(inp, oup, 1, 1, 0, bias=False)
+
+        if expansion == 1:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride,
+                          1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.GELU(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            # 改成Fused-Mbconv
+            self.conv = nn.Sequential(
+
+                # dw
+                nn.Conv2d(inp, hidden_dim, 7, stride, 3,
+                          groups=inp, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.GELU(),
+                # SE(hidden_dim,hidden_dim),
+                # cbam_block(hidden_dim),
+                CA_Block(hidden_dim),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+        self.conv = PreNorm(inp, self.conv, nn.BatchNorm2d)
+
+    def forward(self, x):
+        if self.downsample:
+            return self.proj(self.pool(x)) + self.conv(x)
+        else:
+            return x + self.conv(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, inp, oup, image_size, heads=8, dim_head=32, dropout=0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == inp)
+
+        self.ih, self.iw = image_size
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        # parameter table of relative position bias
+        self.relative_bias_table = nn.Parameter(
+            torch.zeros((2 * self.ih - 1) * (2 * self.iw - 1), heads))
+
+        coords = torch.meshgrid((torch.arange(self.ih), torch.arange(self.iw)))
+        coords = torch.flatten(torch.stack(coords), 1)
+        relative_coords = coords[:, :, None] - coords[:, None, :]
+
+        relative_coords[0] += self.ih - 1
+        relative_coords[1] += self.iw - 1
+        relative_coords[0] *= 2 * self.iw - 1
+        relative_coords = rearrange(relative_coords, 'c h w -> h w c')
+        relative_index = relative_coords.sum(-1).flatten().unsqueeze(1)
+        self.register_buffer("relative_index", relative_index)
+
+        self.attend = nn.Softmax(dim=-1)
+        self.to_qkv = nn.Linear(inp, inner_dim * 3, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, oup),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(
+            t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        # Use "gather" for more efficiency on GPUs
+        relative_bias = self.relative_bias_table.gather(
+            0, self.relative_index.repeat(1, self.heads))
+        relative_bias = rearrange(
+            relative_bias, '(h w) c -> 1 c h w', h=self.ih * self.iw, w=self.ih * self.iw)
+        dots = dots + relative_bias
+
+        attn = self.attend(dots)
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        return out
+
+
+# class FCGA(nn.Module):
+#     def __init__(self, inp, oup, image_size, downsample=True):
+#         super(FCGA, self).__init__()
+#         self.conv1 = nn.Conv2d(inp, inp * 2, kernel_size=1)
+#         self.relu1 = nn.ReLU()
+#         self.conv2 = nn.Conv2d(inp * 2, inp * 2, kernel_size=3, padding=1, stride=2, groups=inp * 2)
+#         self.relu2 = nn.ReLU()
+#
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.relu1(x)
+#         x = self.conv2(x)
+#         x = self.relu2(x)
+#         return x
+
+
+"""    FCGA模块    """
+
+
+class FCGA(nn.Module):
+    def __init__(self, inp, oup, image_size, downsample=True):
+        super(FCGA, self).__init__()
+        # the top route
+        self.conv1 = nn.Conv2d(inp, inp, kernel_size=1, stride=2)
+        self.relu1 = nn.ReLU()
+        # the middle route
+        self.conv2_1 = nn.Conv2d(inp, int(inp / 2), kernel_size=1, stride=2)
+        self.relu2_1 = nn.ReLU()
+        self.conv2_2 = nn.Conv2d(int(inp / 2), int(inp / 2), kernel_size=3, padding=1)
+        self.relu2_2 = nn.ReLU()
+        # the bottom route
+        self.conv3_1 = nn.Conv2d(inp, int(inp / 2), kernel_size=1, stride=2)
+        self.relu3_1 = nn.ReLU()
+        self.conv3_2 = nn.Conv2d(int(inp / 2), int(inp / 2), kernel_size=5, groups=int(inp / 2), padding=2)
+        self.relu3_2 = nn.ReLU()
+
+    def forward(self, x):
+        # the top
+        t = self.conv1(x)
+        t = self.relu1(t)
+        # the middle
+        m = self.conv2_1(x)
+        m = self.relu2_1(m)
+        m = self.conv2_2(m)
+        m = self.relu2_2(m)
+        # the bottom
+        b = self.conv3_1(x)
+        b = self.relu3_1(b)
+        b = self.conv3_2(b)
+        b = self.relu3_2(b)
+        # b c h w
+        y = torch.cat([t, m], dim=1)
+        z = torch.cat([y, b], dim=1)
+        return z
+
+
+class Transformer(nn.Module):
+    def __init__(self, inp, oup, image_size, heads=8, dim_head=32, downsample=False, dropout=0.):
+        super().__init__()
+        hidden_dim = int(inp * 4)
+
+        self.ih, self.iw = image_size
+        self.downsample = downsample
+
+        if self.downsample:
+            self.pool1 = nn.MaxPool2d(3, 2, 1)
+            self.pool2 = nn.MaxPool2d(3, 2, 1)
+            self.proj = nn.Conv2d(inp, oup, 1, 1, 0, bias=False)
+
+        self.attn = Attention(inp, oup, image_size, heads, dim_head, dropout)
+        self.ff = FeedForward(oup, hidden_dim, dropout)
+
+        self.attn = nn.Sequential(
+            Rearrange('b c ih iw -> b (ih iw) c'),
+            PreNorm(inp, self.attn, nn.LayerNorm),
+            Rearrange('b (ih iw) c -> b c ih iw', ih=self.ih, iw=self.iw)
+        )
+        self.ff = nn.Sequential(
+            Rearrange('b c ih iw -> b (ih iw) c'),
+            PreNorm(oup, self.ff, nn.LayerNorm),
+            Rearrange('b (ih iw) c -> b c ih iw', ih=self.ih, iw=self.iw)
+        )
+
+    def forward(self, x):
+        if self.downsample:
+            x = self.proj(self.pool1(x)) + self.attn(self.pool2(x))
+        else:
+            x = x + self.attn(x)
+        x = x + self.ff(x)
+        return x
+
+
+class CoAtNet(nn.Module):
+    def __init__(self, image_size, in_channels, num_blocks, channels, num_classes=1000,
+                 block_types=['C', 'C', 'T', 'T', 'F']):
+        super().__init__()
+        ih, iw = image_size
+        # F代表引导模块
+        block = {'C': MBConv, 'T': Transformer, 'F': FCGA}
+
+        self.s0 = self._make_layer(
+            conv_3x3_bn, in_channels, channels[0], num_blocks[0], (ih // 2, iw // 2))
+        self.s1 = self._make_layer(
+            block[block_types[0]], channels[0], channels[1], num_blocks[1], (ih // 4, iw // 4))
+        self.s5 = self._make_layer(
+            block[block_types[4]], channels[1], channels[2], num_blocks[5], (ih // 4, iw // 4))
+
+        self.s2 = self._make_layer(
+            block[block_types[1]], channels[1], channels[2], num_blocks[2], (ih // 8, iw // 8))
+        self.s6 = self._make_layer(
+            block[block_types[4]], channels[2], channels[3], num_blocks[5], (ih // 8, iw // 8))
+
+        # self.s3 = self._make_layer(
+        #     block[block_types[2]], channels[2], channels[3], num_blocks[3], (ih // 16, iw // 16))
+        # self.s4 = self._make_layer(
+        #     block[block_types[3]], channels[3], channels[4], num_blocks[4], (ih // 32, iw // 32))
+        self.s3 = self._make_layer(
+            block[block_types[2]], channels[2], channels[3], num_blocks[2], (ih // 16, iw // 16))
+        self.s4 = self._make_layer(
+            block[block_types[3]], channels[3], channels[4], num_blocks[4], (ih // 32, iw // 32))
+
+        self.pool = nn.AvgPool2d(ih // 32, 1)
+        self.fc = nn.Linear(channels[4], num_classes, bias=False)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        x = self.s0(x)  # torch.Size([1, 64, 106, 106]) 结果的size
+        x = self.s1(x)  # torch.Size([1, 96, 53, 53])
+        y1 = self.s5(x)
+        x = self.s2(x)
+        y2 = self.s6(x)
+        z1 = y1 + x
+        x = self.s3(z1)
+        z2 = y2 + x
+        x = self.s4(z2)
+        x = self.pool(x).view(-1, x.shape[1])
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
+
+    def _make_layer(self, block, inp, oup, depth, image_size):
+        layers = nn.ModuleList([])
+        for i in range(depth):
+            if i == 0:
+                layers.append(block(inp, oup, image_size, downsample=True))
+            else:
+                layers.append(block(oup, oup, image_size))
+        return nn.Sequential(*layers)
+
+
+def fca_former0():
+    num_blocks = [2, 2, 4, 6, 2, 1]  # L
+    channels = [64, 96, 192, 384, 768, 1536]  # D
+    return CoAtNet((224, 224), 3, num_blocks, channels, num_classes=2)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+if __name__ == '__main__':
+    img = torch.randn(1, 3, 224, 224)
+
+    net = fca_former0()
+    out = net(img)
+    print(net)
+    print(out.shape, count_parameters(net))
